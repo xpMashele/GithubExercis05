db564c9fcae988613ab54d80f886be7cff123e91
RC_Student_lab <RC_Student_lab@10.0.0.11>
2024-08-08 11:58:23 +0000

    First Commit

---
+++ src/github/exercise/GithubExercise.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github.exercise;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class GithubExercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello world!!");
+    }
+    
+}
